0cae996df1ab6b50986b2e5e134a755446801214
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-21 12:17:53 -0500

    Cargando datos de la blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,68 @@ import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
 import Web3 from 'web3';
+import Color from '../abis/Color.json'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('¡Considera usar Metamask!');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    // Cargar una cuenta
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account: accounts[0]});
+    
+    const networkId = '5777' // await web3.eth.net.getId();
+    const networkData = Color.networks[networkId];
+    if(networkData) {
+      const abi = Color.abi;
+      const address = networkData.address;
+      const contract= new web3.eth.Contract(abi, address);
+
+      this.setState({contract});
+
+      // Funcion totalSupply del Smart Contract
+      const totalSupply = contract.methods.totalSupply().call(); // Coger información de la blockchain = call | Enviar informacion a la blockchain = send
+      this.setState({totalSupply});
+
+      // Carga de colores
+      for(var i=1; i <= totalSupply; i++){
+        const color = await contract.methods.colors(i-1).call();
+        this.setState({colors: [...this.state.colors, color]});
+      }
+    } else {
+      window.alert('¡Smart contract no desplegado en la red!');
+    }
+  }
+
+  // Constructor
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      account: '',
+      contract: null,
+      totalSupply: 0,
+      colors: [],
+    }
+  }
+
   render() {
     return (
       <div>
